7073e88cf508312824c2ba220390f0cb0d22cda7
ahmet-dv <95057406+ahmet-dv@users.noreply.github.com>
2022-03-09 02:37:08 +0100

    Add files via upload

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,50 +1,46 @@
 #!/bin/sh
 set -ex
 
-if [ -f "/config/httpd.conf" ]; then
-	ln -sfn /config/httpd.conf $HTTPD_PREFIX/conf/httpd.conf
-else
-	cp $HTTPD_PREFIX/conf/httpd.conf /config/
-fi
-if [ -f "/config/httpd-dav.conf" ]; then
-	ln -sfn /config/httpd-dav.conf $HTTPD_PREFIX/conf/extra/httpd-dav.conf
-else
-	cp $HTTPD_PREFIX/conf/extra/httpd-dav.conf /config/
-fi
-if [ -f "/config/httpd-ssl.conf" ]; then
-	ln -sfn /config/httpd-ssl.conf $HTTPD_PREFIX/conf/extra/httpd-ssl.conf
-else
-	cp $HTTPD_PREFIX/conf/extra/httpd-ssl.conf /config/
-fi
+CONFIGPATH="/config"
 
-
-# Configure vhosts.
-if [ "x$SERVER_NAMES" != "x" ]; then
-    # Use first domain as Apache ServerName.
-    SERVER_NAME="${SERVER_NAMES%%,*}"
+# Set Server Name
+if [ ! -z "$SERVER_NAME" ]; then
     sed -i -e "s|ServerName .*|ServerName $SERVER_NAME|" "$HTTPD_PREFIX/conf/custom/httpd-ssl.conf"
-
-    # Replace commas with spaces and set as Apache ServerAlias.
-    SERVER_ALIAS="`printf '%s\n' "$SERVER_NAMES" | tr ',' ' '`"
-    sed -i -e "/ServerName/a\ \ ServerAlias $SERVER_ALIAS" "$HTTPD_PREFIX/conf/custom/httpd-ssl.conf"
 fi
+
+
+# Copy and link config files
+if [ ! -e "$CONFIGPATH/httpd.conf" ]; then
+	cp $HTTPD_PREFIX/conf/httpd.conf $CONFIGPATH/
+fi
+if [ ! -e "/config/httpd-dav.conf" ]; then
+	cp $HTTPD_PREFIX/conf/extra/httpd-dav.conf $CONFIGPATH/
+fi
+if [ ! -e "/config/httpd-ssl.conf" ]; then
+	cp $HTTPD_PREFIX/conf/extra/httpd-ssl.conf $CONFIGPATH/
+fi
+ln -sfn $CONFIGPATH/httpd.conf $HTTPD_PREFIX/conf/httpd.conf
+ln -sfn $CONFIGPATH/httpd-dav.conf $HTTPD_PREFIX/conf/extra/httpd-dav.conf
+ln -sfn $CONFIGPATH/httpd-ssl.conf $HTTPD_PREFIX/conf/extra/httpd-ssl.conf
+
 
 # Set password hash
-if [ "x$USERNAME" != "x" ] && [ "x$PASSWORD" != "x" ]; then
+if [ ! -z "$USERNAME" ] && [ ! -z "$PASSWORD" ]; then
 	htpasswd -B -b -c "$HTTPD_PREFIX/user.passwd" $USERNAME $PASSWORD
 else
 	htpasswd -B -b -c "$HTTPD_PREFIX/user.passwd" "webdav" "webdav"
 fi
 
-# If not exists, generate a self-signed certificate pair.
-if [ ! -e /key.pem ] || [ ! -e /cert.pem ]; then
+
+# If not exist, generate a self-signed certificate pair.
+if [ ! -e $CONFIGPATH/key.pem ] || [ ! -e $CONFIGPATH/cert.pem ]; then
 	openssl req -x509 -newkey rsa:4096 -days 9999 -nodes \
-	  -keyout /usr/local/apache2/conf/server.key \
-	  -out /usr/local/apache2/conf/server.crt \
+	  -keyout $CONFIGPATH/key.pem \
+	  -out $CONFIGPATH/cert.pem \
 	  -subj "/CN=${SERVER_NAME:-selfsigned}"
 else
-	ln -sfn /key.pem /usr/local/apache2/conf/server.key
-	ln -sfn /cert.pem /usr/local/apache2/conf/server.crt
+	ln -sfn $CONFIGPATH/key.pem /usr/local/apache2/conf/server.key
+	ln -sfn $CONFIGPATH/cert.pem /usr/local/apache2/conf/server.crt
 fi
 
 
